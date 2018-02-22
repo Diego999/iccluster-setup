16521aa01dea3a9dcc2616cf16199e1e2385c47c
Diego999 <diegoantognini@gmail.com>
2018-02-22 10:22:24 +0100

    Update

--- iccluster_setup.sh
+++ iccluster_setup.sh
@@ -7,7 +7,7 @@ apt-get update
 apt-get --assume-yes upgrade
 
 # install python and tools
-apt-get --assume-yes install python3 python3-dev python3-pip
+apt-get --assume-yes install python3 python3-dev python3-pip python3-yaml
 apt-get --assume-yes install git nano screen wget zip unzip g++ htop software-properties-common pkg-config zlib1g-dev
 apt-get --assume-yes install -y gdb cmake cmake-curses-gui autoconf gcc gcc-multilib g++-multilib 
 
@@ -15,19 +15,22 @@ mkdir /home/downloads
 cd /home/downloads
 
 # # download and install CUDA
-VERSION="9.0"
-SUB_VERSION="176"
+VERSION="9.1"
+SUB_VERSION="85"
 SUB_SUB_VERSION="1"
 CUDA_TAR_FILE="cuda-repo-ubuntu1604_${VERSION}.${SUB_VERSION}-${SUB_SUB_VERSION}_amd64.deb"
 curl -O http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/${CUDA_TAR_FILE}
+apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
 dpkg -i ./${CUDA_TAR_FILE}
 apt-get update
-apt-get --assume-yes install cuda=${VERSION}.${SUB_VERSION}-${SUB_SUB_VERSION} -y --allow-unauthenticated # Added this to make sure we don't drag down the newest version of cuda!
+apt-get --assume-yes install cuda
+# For a specific version
+#apt-get --assume-yes install cuda=${VERSION}.${SUB_VERSION}-${SUB_SUB_VERSION} -y --allow-unauthenticated # Added this to make sure we don't drag down the newest version of cuda!
 																										  # Some time the package is not signed with Nvidia GPG. --allow-unauthenticated is a workaround
 
-# download and install libcudnn7, which is necessary tensorflow 1.5 GPU
+# download and install libcudnn
 CUDNN_TAR_FILE="cudnn-${VERSION}-linux-x64-v7.tgz" # Might not work if the token is not available anymore
-wget http://developer2.download.nvidia.com/compute/machine-learning/cudnn/secure/v7.0.5/prod/9.0_20171129/${CUDNN_TAR_FILE}?tPwb9MaNc_aCvnkXenRXq25Amfy0o-E0eQngD5WQOPau6MUr9RcYrQRSzZc9RpF5V93kJr4FOnR7YT-AUVeQuzLYluRtJ-rVJYbKEGtrk_AreGM2ABpQmROr4PaoGvL0M70aY1wBu7u7MGnj473fXw8osiYYRs_KD7TOQKtpVjhFTNSHUAlVWJFkVx3By_OH-p8Yxp1h-04
+wget http://developer2.download.nvidia.com/compute/machine-learning/cudnn/secure/v7.0.5/prod/9.1_20171129/cudnn-9.1-linux-x64-v7.tgz?r0qEHvZ83SnZOvAHNo_H_W05_JexekixSLgWKktpfAJQxzOcJ-Mn8aBhKTaxSMijUfMOdzeZECmzc5FHeL8Dwbht6jl23v7a_gw9ysX1fJuR6fkOSKYDdxXqJRe4GUdTDVHw_YYqawd50rSih-lNUHhjuNJh8guKisrmxLy92UxUMyP6PYGLre0vCEqtRBCiPCq2JQnEla4
 tar -xzvf ${CUDNN_TAR_FILE}
 mkdir /usr/local/cuda-${VERSION}
 mkdir /usr/local/cuda-${VERSION}/lib64
@@ -43,16 +46,22 @@ dd if=/dev/zero of=/swapfile bs=1M count=131072
 mkswap /swapfile
 swapon /swapfile
 
-# python3 as default
-update-alternatives --install /usr/bin/python python /usr/bin/python3 2
-update-alternatives --install /usr/bin/pip pip /usr/bin/pip3 2
-
 # install python packages for machine learning
 yes | pip3 install --upgrade pip
-yes | pip3 install pillow matplotlib mpmath jupyter pandas keras sklearn tensorflow-gpu spacy dill numpy configparser gensim pymysql stanford-corenlp pyrouge cython networkx
+yes | pip3 install pillow matplotlib mpmath jupyter pandas keras sklearn spacy dill numpy configparser gensim pymysql stanford-corenlp cython networkx
 yes | pip3 install -U nltk==3.2.4
-yes | pip3 install http://download.pytorch.org/whl/cu90/torch-0.3.0.post4-cp35-cp35m-linux_x86_64.whl 
+
+yes | pip3 install http://download.pytorch.org/whl/cu91/torch-0.3.1-cp35-cp35m-linux_x86_64.whl
+# or 
+git clone --recursive https://github.com/pytorch/pytorch 
+cd pytorch
+python3 setup.py install
+
 yes | pip3 install torchvision
+
+git clone https://github.com/Diego999/pyrouge
+cd pyrouge
+python3 setup.py install
 
 git clone https://github.com/epfml/sent2vec.git
 cd sent2vec
@@ -67,12 +76,12 @@ pip3 install .
 #exit
 
 # Fix if bug with wordnet
-#cd data/WordNet-2.0-Exceptions/
-#rm WordNet-2.0.exc.db # only if exist
-#./buildExeptionDB.pl . exc WordNet-2.0.exc.db
-#cd ../
-#rm WordNet-2.0.exc.db # only if exist
-#ln -s WordNet-2.0-Exceptions/WordNet-2.0.exc.db WordNet-2.0.exc.db
+cd data/WordNet-2.0-Exceptions/
+rm WordNet-2.0.exc.db # only if exist
+./buildExeptionDB.pl . exc WordNet-2.0.exc.db
+cd ../
+rm WordNet-2.0.exc.db # only if exist
+ln -s WordNet-2.0-Exceptions/WordNet-2.0.exc.db WordNet-2.0.exc.db
 
 # clean up
 cd /home
