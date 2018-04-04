2ecc50f92c3156fdf22a9c4386adc4cff72c3053
Diego999 <diegoantognini@gmail.com>
2018-04-04 14:02:23 +0200

    Update script

--- iccluster_setup.sh
+++ iccluster_setup.sh
@@ -7,9 +7,7 @@ apt-get update
 apt-get --assume-yes upgrade
 
 # install python and tools
-apt-get --assume-yes install python3 python3-dev python3-pip python3-yaml
-apt-get --assume-yes install git nano screen wget zip unzip g++ htop software-properties-common pkg-config zlib1g-dev
-apt-get --assume-yes install -y gdb cmake cmake-curses-gui autoconf gcc gcc-multilib g++-multilib 
+apt-get --assume-yes install python3 python3-dev python3-pip python3-yaml git nano screen wget zip unzip g++ htop software-properties-common pkg-config zlib1g-dev gdb cmake cmake-curses-gui autoconf gcc gcc-multilib g++-multilib 
 
 mkdir /home/downloads
 cd /home/downloads
@@ -23,38 +21,39 @@ curl -O http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_6
 apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
 dpkg -i ./${CUDA_TAR_FILE}
 apt-get update
-apt-get --assume-yes install cuda
-# For a specific version
-#apt-get --assume-yes install cuda=${VERSION}.${SUB_VERSION}-${SUB_SUB_VERSION} -y --allow-unauthenticated # Added this to make sure we don't drag down the newest version of cuda!
-																										  # Some time the package is not signed with Nvidia GPG. --allow-unauthenticated is a workaround
+apt-get --assume-yes install cuda=${VERSION}.${SUB_VERSION}-${SUB_SUB_VERSION}
 
 # download and install libcudnn
-CUDNN_TAR_FILE="cudnn-${VERSION}-linux-x64-v7.tgz" # Might not work if the token is not available anymore
+CUDNN_TAR_FILE="cudnn-${VERSION}-linux-x64-v7.1.tgz" # Might not work if the token is not available anymore
 wget http://developer2.download.nvidia.com/compute/machine-learning/cudnn/secure/v7.0.5/prod/9.1_20171129/cudnn-9.1-linux-x64-v7.tgz?r0qEHvZ83SnZOvAHNo_H_W05_JexekixSLgWKktpfAJQxzOcJ-Mn8aBhKTaxSMijUfMOdzeZECmzc5FHeL8Dwbht6jl23v7a_gw9ysX1fJuR6fkOSKYDdxXqJRe4GUdTDVHw_YYqawd50rSih-lNUHhjuNJh8guKisrmxLy92UxUMyP6PYGLre0vCEqtRBCiPCq2JQnEla4
 tar -xzvf ${CUDNN_TAR_FILE}
 mkdir /usr/local/cuda-${VERSION}
 mkdir /usr/local/cuda-${VERSION}/lib64
 
-# change to 9.0
+# change to 9.1
 sudo cp -P cuda/include/cudnn.h /usr/local/cuda-${VERSION}/include
 sudo cp -P cuda/lib64/libcudnn* /usr/local/cuda-${VERSION}/lib64/
 sudo chmod a+r /usr/local/cuda-${VERSION}/lib64/libcudnn*
 
 # Add 128GB of swap
-swapoff -a
-dd if=/dev/zero of=/swapfile bs=1M count=131072
-mkswap /swapfile
-swapon /swapfile
+#swapoff -a
+#dd if=/dev/zero of=/swapfile bs=1M count=131072
+#mkswap /swapfile
+#swapon /swapfile
 
 # install python packages for machine learning
 yes | pip3 install --upgrade pip
 yes | pip3 install pillow matplotlib mpmath jupyter pandas keras sklearn spacy dill numpy configparser gensim pymysql stanford-corenlp cython networkx
 yes | pip3 install -U nltk==3.2.4
 
-yes | pip3 install http://download.pytorch.org/whl/cu91/torch-0.3.1-cp35-cp35m-linux_x86_64.whl
+#yes | pip3 install http://download.pytorch.org/whl/cu91/torch-0.3.1-cp35-cp35m-linux_x86_64.whl
+
 # or 
 git clone --recursive https://github.com/pytorch/pytorch 
 cd pytorch
+git checkout 2b2d56d8460d335daf5aa79774442a111d424f90
+git submodule update --init
+git submodule update --recursive
 python3 setup.py install
 
 yes | pip3 install torchvision
