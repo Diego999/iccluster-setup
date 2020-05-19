e8ece32c7dd00c88809e8c4b729734dae33e6764
Diego Antognini <diegoantognini@gmail.com>
2020-05-19 09:34:21 +0200

    Update and rename iccluster_setup_cuda9_cudnn7_py36.sh to iccluster_16.04_setup_cuda9_cudnn7_py36.sh

--- iccluster_16.04_setup_cuda9_cudnn7_py36.sh
+++ iccluster_16.04_setup_cuda9_cudnn7_py36.sh
@@ -1,7 +1,6 @@
-sudo apt install software-properties-common
-sudo add-apt-repository ppa:jonathonf/python-3.6
-sudo apt update
-sudo apt install python3.6
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt-get update
+sudo apt-get install python3.6
 
 sudo apt install -y python3 python3.6-dev
 wget https://bootstrap.pypa.io/get-pip.py -O /tmp/get-pip.py
